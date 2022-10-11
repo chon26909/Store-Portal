85a0421c7400a337800dd6a0ab6201935442d0bc
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-11 23:59:58 +0700

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,14 @@
-import React from "react";
+import { FC } from 'react';
 
-const username = "HiChon";
+const username = 'HiChon';
 
-const Header = () => {
-  return (
-    <div className="bg-green flex items-center px-3 justify-between h-[50px]">
-      <div></div>
-      <div className="rounded px-5 py-1 bg-grey font-bold text-sm">
-        {username}
-      </div>
-    </div>
-  );
+const Header: FC = () => {
+    return (
+        <div className='bg-white shadow flex items-center px-3 justify-between h-[50px]'>
+            <div className='text-2xl font-bold ml-2'>Store</div>
+            <div className='rounded px-5 py-1 bg-grey font-bold text-sm'>{username}</div>
+        </div>
+    );
 };
 
 export default Header;
