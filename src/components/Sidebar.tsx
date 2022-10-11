b2093390701b32af3dab1843c51c6eb80e59415d
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-11 23:59:50 +0700

    update sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,41 +8,18 @@ import { ISidebar } from '../types/nav';
 const Sidebar: FC = () => {
     // const { sidebar } = useAppSelector((state) => state);
 
-    // const menu_sidebar = [
-    //     {
-    //         name: 'Dashboard',
-    //         path: '/',
-    //         icon: <i className='fa-solid fa-gauge' />
-    //     },
-    //     {
-    //         name: 'Sensors',
-    //         path: '/sensors',
-    //         icon: <i className='fa-solid fa-temperature-half' />
-    //     },
-    //     {
-    //         name: 'Light',
-    //         path: '/light',
-    //         icon: <i className='fa-solid fa-lightbulb' />
-    //     },
-    //     {
-    //         name: 'Battery',
-    //         path: '/battery',
-    //         icon: <i className='fa-solid fa-car-battery' />
-    //     }
-    // ];
-
     const menu_sidebar: ISidebar[] = [
         {
             name: 'Dashboard',
-            path: 'dashboard'
+            path: '/dashboard'
         },
         {
             name: 'Product',
-            path: 'product'
+            path: '/product'
         },
         {
             name: 'Manage User',
-            path: 'manage-user'
+            path: '/manage-user'
         }
     ];
 
@@ -54,7 +31,6 @@ const Sidebar: FC = () => {
                         <NavLink
                             to={item.path}
                             className={({ isActive }) => {
-                                console.log(item.path, isActive);
                                 return [styles.Item, isActive ? styles.Active : ''].join(' ');
                             }}
                             key={index}
@@ -71,115 +47,3 @@ const Sidebar: FC = () => {
 };
 
 export default Sidebar;
-
-// import { FC } from 'react';
-// import { NavLink } from 'react-router-dom';
-// import styled from 'styled-components';
-// import styles from '../styles/sidebar.module.scss';
-// import { ISidebar } from '../types/nav';
-
-// const sidebar: ISidebar[] = [
-//     {
-//         name: 'Dashboard',
-//         path: '/'
-//     },
-//     {
-//         name: 'Product',
-//         path: '/product'
-//     },
-//     {
-//         name: 'Manage User',
-//         path: '/manage-user'
-//     }
-// ];
-
-// const Sidebar: FC = () => {
-//     return (
-//         <nav className='fixed w-[200px] bg-primary h-full'>
-//             <div className='text-center font-bold text-2xl m-2 text-white'>POS</div>
-//             <List>
-//                 {sidebar.map((item, i) => {
-//                     return (
-//                         <NavLink
-//                             to={item.path}
-//                             className={({ isActive }) => {
-//                                 console.log(isActive);
-//                                 return ['bg-red', isActive ? 'font-thin' : ''].join(' ');
-//                             }}
-//                             key={i}
-//                         >
-//                             <Item>
-//                                 <div></div>
-//                                 <div>{item.name}</div>
-//                             </Item>
-//                         </NavLink>
-//                     );
-//                 })}
-//             </List>
-//         </nav>
-//     );
-// };
-
-// export default Sidebar;
-
-// const List = styled.div`
-//     padding: 5px;
-// `;
-
-// const Item = styled.div`
-//     font-size: 18px;
-//     color: white;
-//     padding-left: 25px;
-//     width: 100%;
-//     display: block;
-//     border-top-left-radius: 30px;
-//     border-bottom-left-radius: 30px;
-//     position: relative;
-//     display: grid;
-//     grid-template-columns: 40px 1fr;
-//     align-items: center;
-//     height: 50px;
-
-//     &.Active {
-//         color: var(--primary-color);
-//         background-color: white;
-
-//         &::before {
-//             content: '';
-//             width: 30px;
-//             height: 30px;
-//             background: transparent;
-//             position: absolute;
-//             top: -30px;
-//             right: 0px;
-//             border-radius: 50%;
-//             box-shadow: 25px 25px 0 10px white;
-//         }
-
-//         &::after {
-//             content: '';
-//             width: 30px;
-//             height: 30px;
-//             background: transparent;
-//             position: absolute;
-//             bottom: -30px;
-//             right: 0px;
-//             border-radius: 50%;
-//             box-shadow: 25px -25px 0 10px white;
-//         }
-//     }
-
-//     &:not(.Active):hover {
-//         padding-left: 28px;
-//         transition: all ease-in-out 0.2s;
-//         background-color: rgba(255, 255, 255, 0.15);
-//     }
-
-//     & > div:nth-child(1) {
-//         text-align: center;
-
-//         i {
-//             font-size: 26px;
-//         }
-//     }
-// `;
