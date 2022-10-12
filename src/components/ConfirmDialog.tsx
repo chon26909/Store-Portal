b0a54a9056db32e9f2a492157a5c803a5874c52a
Chonlasit <chonlasit.s@arise.tech>
2022-10-12 21:40:10 +0700

    create dialog

---
+++ src/components/ConfirmDialog.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import styles from '../styles/modal.module.scss';
+import Button from './Button';
+
+interface IConfirmDialogProps {
+    isOpen: boolean;
+    title: string;
+    message?: string;
+    icon?: 'success' | 'danger' | 'warning' | undefined;
+    onConfirm: () => void;
+    onCancel: () => void;
+    // onClose: (state: boolean) => void;
+}
+
+const IconDialog = ({ icon }: { icon: IConfirmDialogProps['icon'] }) => {
+    if (icon === 'success') return <i className='fas fa-circle-check text-green' />;
+    else if (icon === 'warning') return <i className='fas fa-circle-exclamation text-primary ' />;
+    else if (icon === 'danger') return <i className='fas fa-circle-exclamation text-red' />;
+    else return <i></i>;
+};
+
+const ConfirmDialog: FC<IConfirmDialogProps> = (props) => {
+    const { isOpen, title, message, icon, onConfirm, onCancel } = props;
+    return (
+        <AnimatePresence>
+            {isOpen ? (
+                <motion.div className={[styles.ModalDackdrop].join(' ')} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+                    <motion.div className={styles.ModalConfirmDialog} initial={{ scale: 0 }} animate={{ scale: 1 }}>
+                        <div className='text-center text-[55px]'>
+                            <IconDialog icon={icon} />
+                        </div>
+
+                        <div className='text-center text-2xl font-bold'>{title}</div>
+                        <div className='text-center'>{'' + message}</div>
+                        <div className='text-center mt-3'>
+                            <Button className='mx-2 px-[4rem] py-3 bg-tranperent border-grey border-[1px] box-border border=[1px] text-black' onClick={onCancel}>
+                                ยกเลิก
+                            </Button>
+                            <Button className={[icon ? 'bg-red border-red' : '', 'mx-2 px-[4rem] py-3 border-primary border-[1px]'].join(' ')} onClick={onConfirm}>
+                                ยืนยัน
+                            </Button>
+                        </div>
+                    </motion.div>
+                </motion.div>
+            ) : null}
+        </AnimatePresence>
+    );
+};
+
+export default ConfirmDialog;
