0260416bca0078dc9c32a176dbbcffbe7a921335
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-15 00:16:52 +0700

    update product

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,13 +1,13 @@
 import { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Button from '../components/Button';
-import CardProduct from '../components/CardProduct';
+import { IProduct } from '../types/product';
 import Dialog from '../components/Dialog';
 import ModalProductAdd from '../components/ModalProductAdd';
 import Title from '../components/Title';
 import productStore from '../store/productStore';
 
-const ProductPage: FC = () => {
+const ProductPage = () => {
     const [isOpenModalAdd, setIsOpenModalAdd] = useState(false);
 
     const { data, fetchProducts } = productStore();
@@ -21,14 +21,14 @@ const ProductPage: FC = () => {
     };
 
     return (
-        <>
+        <div>
             <Title>สินค้าทั้งหมด</Title>
             <Button type='button' onClick={() => setIsOpenModalAdd(true)}>
                 เพิ่มสินค้า
             </Button>
             <ProductList>{data && data.map((item, i) => <CardProduct {...item} key={i} />)}</ProductList>
             <ModalProductAdd isOpen={isOpenModalAdd} onClose={() => setIsOpenModalAdd(false)} />
-        </>
+        </div>
     );
 };
 
@@ -39,4 +39,30 @@ const ProductList = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
     grid-gap: 1rem;
+`;
+
+//---------------------------------------------------------------- //
+
+interface ICardProductProps extends IProduct {
+    className?: string;
+}
+
+const CardProduct: FC<ICardProductProps> = (props) => {
+    return (
+        <div className='bg-white p-4 border-[#e8e8e8] rounded-lg shadow-xl cursor-pointer hover:scale-105'>
+            <Photo src={props.photo} />
+            <div className='font-bold text-lg'>{props.title}</div>
+            <div className='text-gray_text'>{props.desc.length ? props.desc.substring(0, 50) + '...' : props.desc}</div>
+            <div className='text-md'>ราคา {props.price} บาท</div>
+            <div className='text-md'>คงเหลือ {props.qty} ชิ้น</div>
+        </div>
+    );
+};
+
+const Photo = styled.img`
+    object-fit: cover;
+    object-position: center;
+    width: 100%;
+    height: 180px;
+    margin-bottom: 10px;
 `;
