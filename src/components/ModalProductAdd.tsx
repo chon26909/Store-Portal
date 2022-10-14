aeef4b353a03a6d31ed0db3331eeac90f8610214
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-15 00:17:51 +0700

    update modal product

--- src/components/ModalProductAdd.tsx
+++ src/components/ModalProductAdd.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from 'react';
 import Button from './Button';
 import Dialog from './Dialog';
+import Input from './Input';
 import Modal from './Modal';
 import Title from './Title';
 
@@ -17,7 +18,27 @@ const ModalProductAdd: FC<IModalProps> = (props) => {
     return (
         <Modal isOpen={isOpen}>
             <Title>เพิ่มสินค้า</Title>
-            <Button onClick={() => setIsOpenConfirm(true)}>ยกเลิก</Button>
+            <div>
+                <Input type='text' label='ชื่อสินค้า' placeholder='' />
+            </div>
+            <div>
+                <Input type='text' label='รายละเอียดสินค้า' placeholder='' />
+            </div>
+            <div>
+                <Input type='text' label='ราคา' placeholder='' />
+            </div>
+            <div>
+                <Input type='text' label='จำนวน' placeholder='' />
+            </div>
+
+            <div className='absolute bottom-0 right-0 p-7 w-full'>
+                <div className='flex justify-end'>
+                    <Button className='mr-3 bg-tranperent text-black' onClick={() => setIsOpenConfirm(true)}>
+                        ยกเลิก
+                    </Button>
+                    <Button onClick={() => setIsOpenConfirm(true)}>เพิ่มสินค้า</Button>
+                </div>
+            </div>
             <Dialog
                 icon='warning'
                 isOpen={isOpenConfirm}
