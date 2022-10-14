289e23f368d5b12e4a46d58ad85b00481af68f90
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-15 00:32:14 +0700

    update user store

--- src/pages/ManageUserPage.tsx
+++ src/pages/ManageUserPage.tsx
@@ -1,51 +1,33 @@
 import { FC, useEffect } from 'react';
 import Table, { TableBody, TableHeader, TableRow } from '../components/Table';
 import Title from '../components/Title';
+import userStore from '../store/userStore';
 
 const ManageUserPage: FC = () => {
+    const { data, loading, fetchData } = userStore();
+
     const columns = ['No.', 'Email', 'Role'];
 
-    let dataSource = [
-        {
-            email: 'chon@gmail.com',
-            role: 'admin'
-        },
-        {
-            email: 'chon@gmail.com',
-            role: 'admin'
-        },
-        {
-            email: 'chon@gmail.com',
-            role: 'admin'
-        },
-        {
-            email: 'chon@gmail.com',
-            role: 'admin'
-        },
-        {
-            email: 'chon@gmail.com',
-            role: 'admin'
-        }
-    ];
-
-    useEffect(() => {}, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <div>
             <Title>ข้อมูลผู้ใช้งานระบบ และสิทธิการเข้าถึง</Title>
             <FilterUser />
-            <Table loading={false}>
+            <Table loading={loading}>
                 <TableHeader>
                     {columns.map((column, i) => (
                         <th key={i}>{column}</th>
                     ))}
                 </TableHeader>
                 <TableBody>
-                    {dataSource.map((row, i) => (
-                        <TableRow>
-                            <td key={i}>{i + 1}</td>
-                            <td key={i}>{row.email}</td>
-                            <td key={i}>{row.role}</td>
+                    {data.map((row, i) => (
+                        <TableRow key={i}>
+                            <td>{i + 1}</td>
+                            <td>{row.email}</td>
+                            <td>{row.role}</td>
                         </TableRow>
                     ))}
                 </TableBody>
---
+++ src/store/userStore.ts
@@ -0,0 +1,44 @@
+import zustand from 'zustand';
+import { IUser } from '../types/uset';
+
+interface IUserStore {
+    data: IUser[];
+    loading: boolean;
+    fetchData: () => void;
+}
+
+const userStore = zustand<IUserStore>()((set) => ({
+    data: [],
+    loading: true,
+    fetchData: () => {
+        setTimeout(() => {
+            set(() => ({
+                loading: false,
+                data: [
+                    {
+                        email: 'chon@gmail.com',
+                        role: 'admin'
+                    },
+                    {
+                        email: 'chon@gmail.com',
+                        role: 'admin'
+                    },
+                    {
+                        email: 'chon@gmail.com',
+                        role: 'admin'
+                    },
+                    {
+                        email: 'chon@gmail.com',
+                        role: 'admin'
+                    },
+                    {
+                        email: 'chon@gmail.com',
+                        role: 'admin'
+                    }
+                ]
+            }));
+        }, 2000);
+    }
+}));
+
+export default userStore;
