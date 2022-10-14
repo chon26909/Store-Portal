e7dfe544b52cd3bb1a7804c40220e6147bcf98c5
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-15 00:17:23 +0700

    update input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,64 +1,60 @@
-import {
-  ForwardRefRenderFunction,
-  InputHTMLAttributes,
-  FC,
-  useState,
-} from "react";
-
-import EyeIcon from "../assets/icons/eye.svg";
-import EyeHideIcon from "../assets/icons/eye-hide.svg";
-
+import { ForwardRefRenderFunction, InputHTMLAttributes, FC, useState } from 'react';
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  type: "text" | "password";
-  label: string;
-  error?: boolean;
-  errorMessage?: string;
+    type: 'text' | 'password';
+    label: string;
+    errorMessage?: string;
+    full?: boolean;
 }
-const InputText = ({ label }: Props) => {
-  return (
-    <div>
-      <label htmlFor="text" className="block">
-        {label}
-      </label>
-      <input
-        type="text"
-        className="w-[250px] mt-1 px-1 py-1 bg-white border shadow-sm border-gray-400 rounded-sm placeholder-slate-400 
-         focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
-      />
-    </div>
-  );
-};
-const InputPassword: FC<Props> = ({ label }) => {
-  const [show, setShow] = useState(false);
 
-  return (
-    <div>
-      <label htmlFor="text" className="block">
-        {label}
-      </label>
-      <div className="relative inline-block">
-        <input
-          type={show ? "text" : "password"}
-          className="w-[250px] mt-1 pl-1 pr-10 py-1 bg-white border shadow-sm border-gray-400 rounded-sm placeholder-slate-400 
-         focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
-        />
-        <div
-          className="absolute top-3 right-3 cursor-pointer"
-          onClick={() => setShow((s) => !s)}
-        >
-          <img
-            src={show ? EyeIcon : EyeHideIcon}
-            className="w-[25px] h-[25px]"
-            alt="eye"
-          />
+const inputClass = 'text-[18px] mt-1 px-3 py-2 bg-white border shadow-sm border-gray_text rounded placeholder-slate-400 focus:outline-none';
+
+const InputText = ({ label, full, errorMessage, ...other }: Props) => {
+    return (
+        <div className={[full ? 'w-full' : 'inline-block', ' mt-2 mr-3'].join(' ')}>
+            <label htmlFor='text' className='block'>
+                {label}
+            </label>
+            <input
+                {...other}
+                type='text'
+                className={[full ? 'w-full' : 'w-[250px]', errorMessage && errorMessage.length > 0 ? '!border-red ring-1 ring-red' : 'focus:border-primary focus:ring-1 ring-primary', inputClass].join(
+                    ' '
+                )}
+            />
+            {errorMessage && errorMessage.length > 0 ? <div className='text-red'>{errorMessage + ' '}&nbsp;</div> : undefined}
         </div>
-      </div>
-    </div>
-  );
+    );
+};
+const InputPassword: FC<Props> = ({ label, full, errorMessage, ...other }) => {
+    const [show, setShow] = useState(false);
+
+    return (
+        <div className={[full ? 'w-full' : 'inline-block', 'mt-2 mr-3'].join(' ')}>
+            <label htmlFor='text' className='block'>
+                {label}
+            </label>
+            <div className='relative'>
+                <input
+                    {...other}
+                    type={show ? 'text' : 'password'}
+                    className={[
+                        full ? 'w-full' : 'w-[250px]',
+                        show ? '' : 'tracking-[4px]',
+                        errorMessage && errorMessage.length > 0 ? '!border-red ring-1 ring-red' : 'focus:border-primary focus:ring-1 ring-primary',
+                        inputClass
+                    ].join(' ')}
+                />
+                <div className='text-gray_text absolute top-1.5 right-2 cursor-pointer w-8 h-8 flex items-center justify-center text-[20px]' onClick={() => setShow((s) => !s)}>
+                    {show ? <i className='fas fa-eye ' /> : <i className='fas fa-eye-slash' />}
+                </div>
+            </div>
+            {errorMessage && errorMessage.length > 0 ? <div className='text-red'>{errorMessage + ' '}&nbsp;</div> : undefined}
+        </div>
+    );
 };
 const Input: ForwardRefRenderFunction<HTMLInputElement, Props> = (props) => {
-  if (props.type === "password") return <InputPassword {...props} />;
-  return <InputText {...props} />;
+    if (props.type === 'password') return <InputPassword {...props} />;
+    return <InputText {...props} />;
 };
 
 export default Input;
