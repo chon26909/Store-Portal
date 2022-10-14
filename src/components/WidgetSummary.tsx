99193ba27fcd991fd98cd448f5fa1970d55eae7b
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-15 00:18:30 +0700

    update widget

--- src/components/WidgetSummary.tsx
+++ src/components/WidgetSummary.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { FC } from 'react';
 
-const WidgetSummary = () => {
+interface IWidget {
+    title: string;
+    count: string | number;
+    className?: string;
+}
+
+const WidgetSummary: FC<IWidget> = ({ title, count, className }) => {
     return (
-        <div className='rounded shadow-grey border inline-block p-3 bg-green'>
-            <div>ผู้ใช้งาน</div>
+        <div className={[className, 'min-w-[250px] mr-3 mb-3 rounded shadow-gray inline-block p-7'].join(' ')}>
+            <div className='text-white text-lg'>{title}</div>
+            <div className='text-white font-extrabold text-3xl'>{count}</div>
         </div>
     );
 };
