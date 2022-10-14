f92aca77e28b085871443e5e80a23f7c0c96db0e
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-15 00:18:08 +0700

    create suctom table

---
+++ src/components/Table.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+import LoadingIcon from './LoadingIcon';
+
+interface ITable {
+    children: JSX.Element[];
+    loading: boolean;
+}
+
+const Table: FC<ITable> = ({ children, loading }) => {
+    return (
+        <div className='min-h-[500px] overflow-x-auto bg-white rounded overflow-hidden border-[1px] border-[#ddd] relative'>
+            <table className='w-full border-gray'>{children}</table>
+            <OverlayLoading loading={loading} />
+        </div>
+    );
+};
+
+export const TableHeader: FC<{ children: JSX.Element[] }> = ({ children }) => {
+    return (
+        <thead className='text-white text-left bg-primary'>
+            <tr className='[&>th]:px-5 [&>th]:py-4'>{children}</tr>
+        </thead>
+    );
+};
+
+export const TableBody: FC<{ children: JSX.Element[] }> = ({ children }) => <tbody>{children}</tbody>;
+
+export const TableRow: FC<{ children: JSX.Element[] }> = ({ children }) => {
+    return <tr className='[&>td]:px-5 [&>td]:py-3 border-b-[#f2f2f2] border-b-[1px] even:bg-[#f2f2f2]'>{children}</tr>;
+};
+
+const OverlayLoading: FC<{ loading: boolean }> = ({ loading }) => {
+    if (loading) {
+        return (
+            <div className='absolute top-0 left-0 right-0 bottom-0 bg-[#00000030] flex items-center justify-center select-none cursor-progress'>
+                <div className='flex flex-col items-center'>
+                    <LoadingIcon />
+                    <div className='text-white'>กำลังโหลดข้อมูล</div>
+                </div>
+            </div>
+        );
+    }
+    return <div></div>;
+};
+
+export default Table;
