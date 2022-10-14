0341ff7361fbf37bede32ed0fa4c78e3321e6aac
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-15 00:17:14 +0700

    update coler header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ const username = 'HiChon';
 
 const Header: FC = () => {
     return (
-        <div className='bg-white shadow flex items-center px-3 justify-between h-[60px]'>
+        <div className='bg-white border-b-[#c4c4c4] border-b-[1px] flex items-center px-3 justify-between h-[60px]'>
             <div className='text-3xl font-bold ml-1'>Store</div>
-            <div className='rounded px-5 py-2 bg-grey shadow cursor-pointer select-none'>
+            <div className='rounded px-5 py-2 cursor-pointer select-none'>
                 <i className='fas fa-circle-user text-xl' />
                 <span className='ml-2 text-xl font-bold '>{username}</span>
             </div>
