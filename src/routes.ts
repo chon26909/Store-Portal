f679bbcad661f8791a4a376f9ca08bef36a2719a
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-15 00:16:28 +0700

    update route

---
+++ src/routes.ts
@@ -0,0 +1,43 @@
+import { FC, lazy, LazyExoticComponent } from 'react';
+import CustomersPage from './pages/CustomersPage';
+import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import ManageUserPage from './pages/ManageUserPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import ProductPage from './pages/ProductPage';
+
+interface IRoutesType {
+    [key: string]: { path: string; element: FC }[];
+}
+
+const routes: IRoutesType = {
+    public: [
+        {
+            path: '/login',
+            element: LoginPage
+        }
+    ],
+    private: [
+        {
+            path: '/dashboard',
+            element: DashboardPage
+        },
+        {
+            path: '/products',
+            element: ProductPage
+        },
+        {
+            path: '/product/:id',
+            element: ProductDetailPage
+        },
+        {
+            path: '/customers',
+            element: CustomersPage
+        },
+        {
+            path: '/role',
+            element: ManageUserPage
+        }
+    ]
+};
+export default routes;
