ceb53f780abd81a84db62202c5116189b1669648
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-22 21:31:05 +0700

    update manage user

--- src/pages/ManageUserPage.tsx
+++ src/pages/ManageUserPage.tsx
@@ -1,5 +1,9 @@
 import { FC, useEffect } from 'react';
-import Table, { TableBody, TableHeader, TableRow } from '../components/Table';
+import Button from '../components/Button';
+import Dropdown from '../components/Dropdown';
+import Input from '../components/Input';
+import Modal from '../components/Modal';
+import Table, { TableBody, TableColumn, TableHead, TableHeader, TableRow } from '../components/Table';
 import Title from '../components/Title';
 import userStore from '../store/userStore';
 
@@ -14,30 +18,67 @@ const ManageUserPage: FC = () => {
 
     return (
         <div>
-            <Title>ข้อมูลผู้ใช้งานระบบ และสิทธิการเข้าถึง</Title>
+            <div className='flex justify-between mb-2'>
+                <Title>ข้อมูลผู้ใช้งานระบบ และสิทธิการเข้าถึง</Title>
+                <Button>เพิ่มผู้ใช้งาน</Button>
+            </div>
             <FilterUser />
             <Table loading={loading}>
                 <TableHeader>
                     {columns.map((column, i) => (
-                        <th key={i}>{column}</th>
+                        <TableHead key={i}>{column}</TableHead>
                     ))}
                 </TableHeader>
                 <TableBody>
                     {data.map((row, i) => (
                         <TableRow key={i}>
-                            <td>{i + 1}</td>
-                            <td>{row.email}</td>
-                            <td>{row.role}</td>
+                            <TableColumn>{i + 1}</TableColumn>
+                            <TableColumn>{row.email}</TableColumn>
+                            <TableColumn>{row.role}</TableColumn>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
+            <ModalCreateUser />
         </div>
     );
 };
 
 const FilterUser = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div className='my-3'>
+                <Input type='text' label='Email' placeholder='' />
+            </div>
+            <div className='my-3'>
+                <Button>ค้นหา</Button>
+                <Button className='bg-tranperent shadow-none text-black'>รีเซ็ต</Button>
+            </div>
+        </div>
+    );
+};
+
+const ModalCreateUser = () => {
+    const options = [
+        {
+            label: 'Admin',
+            value: 'Admin'
+        },
+        {
+            label: 'Sale',
+            value: 'Sale'
+        }
+    ];
+
+    return (
+        <Modal isOpen={false}>
+            <Title>เพิ่มผู้ใช้งาน</Title>
+            <div>
+                <Input type='text' label='Email' />
+                <Dropdown options={options} label='สิทธิผู้ใช้งาน' />
+            </div>
+        </Modal>
+    );
 };
 
 export default ManageUserPage;
