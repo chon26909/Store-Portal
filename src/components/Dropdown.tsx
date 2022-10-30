ac1c612a514946d35a787172c9a914b83b44e982
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-10-31 00:45:15 +0700

    update downdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,9 +1,8 @@
-import { FC, createRef, useRef, useState, useEffect } from 'react';
+import { FC, useState, useEffect } from 'react';
 
 interface IDropdown {
     options: { label: string; value: string }[];
     label: string;
-    placeholder: string;
     full?: boolean;
     value: string;
     onChange: (value: string) => void;
@@ -11,15 +10,12 @@ interface IDropdown {
 
 const SelectClass = 'w-[250px] text-[18px] mt-1 px-3 py-2 bg-white border shadow-sm border-gray_text rounded placeholder-slate-400 focus:outline-none';
 
-const OptionClass = 'py-5 h-5 bg-green';
+const OptionClass = 'px-3 py-2 hover:bg-[#e7eefa]';
 
-const Dropdown: FC<IDropdown> = ({ options, label, value, placeholder, full, onChange }) => {
-    // const container = useRef(null);
-
+const Dropdown: FC<IDropdown> = ({ options, label, value, full, onChange }) => {
     const [showMenu, setShowMenu] = useState(false);
 
     const handleClickOpen = (e: any) => {
-        console.log('open menu');
         e.stopPropagation();
         setShowMenu(true);
     };
@@ -28,15 +24,10 @@ const Dropdown: FC<IDropdown> = ({ options, label, value, placeholder, full, onC
         const result = options.find((item) => {
             return item.value === value;
         });
-        return result?.label ? result.label : placeholder;
+        return result?.label;
     };
 
-    // const onChange = () => {
-    //     return (value = selected);
-    // };
-
     useEffect(() => {
-        console.log('close menu');
         const handle = () => setShowMenu(false);
         window.addEventListener('click', handle);
 
@@ -48,20 +39,23 @@ const Dropdown: FC<IDropdown> = ({ options, label, value, placeholder, full, onC
     };
 
     return (
-        <div className='my-2 relative w-[250px]'>
-            <div className={SelectClass} onClick={(e) => handleClickOpen(e)}>
-                <div className=''>{getLable()}</div>
-                <div className='absolute top-2 right-4'>{showMenu ? <i className='fa-solid fa-chevron-up' /> : <i className='fa-solid fa-chevron-down' />}</div>
-            </div>
-            {showMenu ? (
-                <div className='bg-white w-full absolute top-[45px] border shadow-lg border-gray_text'>
-                    {options.map((item, index) => (
-                        <div key={index} className='px-3 py-2 hover:bg-[#e7eefa]' onClick={() => changeOption(item.value)}>
-                            {item.label}
-                        </div>
-                    ))}
+        <div className='my-2 w-[250px]'>
+            <div>{label}</div>
+            <div className='relative'>
+                <div className={SelectClass} onClick={(e) => handleClickOpen(e)}>
+                    <div className=''>{getLable()}</div>
+                    <div className='absolute top-2 right-4'>{showMenu ? <i className='fa-solid fa-chevron-up' /> : <i className='fa-solid fa-chevron-down' />}</div>
                 </div>
-            ) : null}
+                {showMenu ? (
+                    <div className='bg-white w-full absolute top-[45px] border shadow-lg border-gray_text'>
+                        {options.map((item, index) => (
+                            <div key={index} className={OptionClass} onClick={() => changeOption(item.value)}>
+                                {item.label}
+                            </div>
+                        ))}
+                    </div>
+                ) : null}
+            </div>
         </div>
     );
 };
