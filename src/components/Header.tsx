b1564138138e62b0ef484d1d048784682a916208
Chonlasit <chonlasit.s@arise.tech>
2022-11-13 21:52:16 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const username = 'HiChon';
 const Header: FC = () => {
     return (
         <div className='bg-white border-b-[#c4c4c4] border-b-[1px] flex items-center px-3 justify-between h-[60px]'>
-            <div className='text-3xl font-bold ml-1'>Store</div>
+            <div className='text-3xl font-bold ml-1'>Online Store</div>
             <div className='rounded px-5 py-2 cursor-pointer select-none'>
                 <i className='fas fa-circle-user text-xl' />
                 <span className='ml-2 text-xl font-bold '>{username}</span>
--- src/pages/ManageUserPage.tsx
+++ src/pages/ManageUserPage.tsx
@@ -8,13 +8,14 @@ import Select from '../components/Select';
 import ImagePicker from '../components/ImagePicker';
 import { addUser, getUsers } from '../store/slices/userSlice';
 import { useAppDispatch, useAppSelector } from '../store';
+import { USER_STATUS } from '../types/user';
 
 const ManageUserPage: FC = () => {
     const dispatch = useAppDispatch();
     const { data, loading } = useAppSelector((s) => s.users);
     const [isOpenModalCreate, setIsOpenModalCreate] = useState(false);
 
-    const columns = ['No.', 'Email', 'Role'];
+    const columns = ['No.', 'ชื่อ นามสกุล', 'อีเมล', 'สิทธิการเข้าถึง', 'สถานะ'];
 
     useEffect(() => {
         dispatch(getUsers());
@@ -36,9 +37,13 @@ const ManageUserPage: FC = () => {
                 <TableBody>
                     {data.map((row, i) => (
                         <TableRow key={i}>
-                            <TableColumn>{i + 1}</TableColumn>
+                            <TableColumn className='w-[60px]'>{i + 1}</TableColumn>
+                            <TableColumn>{row.firstname + ' ' + row.lastname}</TableColumn>
                             <TableColumn>{row.email}</TableColumn>
                             <TableColumn>{row.role}</TableColumn>
+                            <TableColumn className='w-[170px]'>
+                                <UserStatus status={row.user_status} />
+                            </TableColumn>
                         </TableRow>
                     ))}
                 </TableBody>
@@ -46,6 +51,18 @@ const ManageUserPage: FC = () => {
             <ModalCreateUser isOpen={isOpenModalCreate} onClose={() => setIsOpenModalCreate(false)} />
         </div>
     );
+};
+
+const UserStatus: FC<{ status: string }> = ({ status }) => {
+    const style = 'py-2 px-4 rounded-[20px] inline-block text-white text-[14px]';
+    if (status === USER_STATUS.ENABLED) {
+        return <div className={[style, 'bg-green'].join(' ')}>เปิดการใช้งาน</div>;
+    } else if (status === USER_STATUS.DISABLED) {
+        return <div className={[style, 'bg-red'].join(' ')}>ปิดการใช้งาน</div>;
+    } else if (status === USER_STATUS.SUSPENDED) {
+        return <div className={[style, 'bg-yellow'].join(' ')}>ระงับการใช้งาน</div>;
+    }
+    return <div></div>;
 };
 
 const FilterUser = () => {
