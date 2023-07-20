8f2285017bccf65acd617afc99c263e6ad95ed38
HiChon <59905927+chon26909@users.noreply.github.com>
2023-07-20 23:19:37 +0700

    add effect modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 interface IModalProps {
     children: JSX.Element | JSX.Element[];
     isOpen: boolean;
@@ -8,48 +8,55 @@ interface IModalProps {
 }
 
 const Modal: FC<IModalProps> = ({ children, isOpen, modalClassName, bodyClassName }) => {
-    if (!isOpen) return null;
+    const [isOverlay, setIsOverlay] = useState(false);
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsOverlay(true);
+            setTimeout(() => {
+                setShow(true);
+            }, 100);
+        } else {
+            setShow(false);
+            setTimeout(() => {
+                setIsOverlay(false);
+            }, 270);
+        }
+    }, [isOpen]);
+
     return (
         <div
-            className={`fixed inset-0 z-50 bg-black/50 
-                        flex items-center justify-center 
-                        overflow-x-hidden 
-                        focus:outline outline-none`}
+            className={`${
+                isOverlay ? 'custom-modal fixed inset-0 z-50 bg-black/50 flex items-center justify-center focus:outline outline-none' : ''
+            } `}
         >
             <div
                 className={`${modalClassName}
                             mx-auto 
-                            h-3/4 w-[1000px] 
+                            h-[600px] w-[1000px] 
+                            lg:h-[600px] lg:w-4/6
                             md:h-full md:w-full 
-                            lg:h-4/5 lg:w-4/6
-
-                            ${isOpen ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}
+                          
                             relative rounded-md md:rounded-none
-                            transition duration-300
+                            transition duration-200
+                            ${show ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}
+                            
                             bg-white
+                            overflow-hidden
                             `}
             >
-                <div className={[bodyClassName, ' h-full overflow-y-auto '].join(' ')}>{children}</div>
-
-                {/* <div
-                    className={`
-                        rounded-md
-                        
-                        bg-white 
-                        h-full
-                        transition
-                        duration-300
-                        overflow-hidden
-                        ${isOpen ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}
-                    `}
-                >
-                    <div className=''>
-                        <div className=' bg-white '>{children}</div>
-                    </div>
-                </div> */}
+                <div className={[bodyClassName, 'translate h-full '].join(' ')}>{children}</div>
             </div>
         </div>
     );
+};
+
+export const ModalHeader: FC<{ children: JSX.Element; className?: string }> = ({ children, className }) => {
+    return <div className={[className, 'h-[70px] p-5 border-b-2 border-b-[#dedede] text-xl font-semibold'].join(' ')}>{children}</div>;
+};
+export const ModalFooter: FC<{ children: JSX.Element; className?: string }> = ({ children, className }) => {
+    return <div className={[className, 'h-[80px] p-5'].join(' ')}>{children}</div>;
 };
 
 export default Modal;
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -3,11 +3,15 @@ import Switch from '../components/Switch';
 import WidgetSummary from '../components/WidgetSummary';
 import InputFile from '../components/InputFile';
 import Chart from '../components/Chart';
-import Modal from '../components/Modal';
+import Modal, { ModalHeader } from '../components/Modal';
+import Button from '../components/Button';
 
 const DashboardPage = () => {
     const [toggle, settoggle] = useState(false);
     const [file, setFile] = useState<File>();
+
+    const handleCloseModal = () => settoggle(false);
+
     return (
         <div>
             {/* <WidgetSummary title='ผู้ใช้งานทั้งหมด' count={1200} className='bg-primary' />
@@ -23,11 +27,31 @@ const DashboardPage = () => {
 
             <InputFile onChange={(value) => setFile(value)} acceptFile={['CSV']} />
             {/* <Chart /> */}
-            <Modal isOpen={true} modalClassName='p-5'>
-                <div>
-                    {[...Array(100)].map(() => (
-                        <div>A</div>
-                    ))}
+
+            <Button onClick={() => settoggle(true)}>เพิ่มสินค้า</Button>
+
+            <Modal isOpen={toggle} bodyClassName=''>
+                <ModalHeader>
+                    <div>เพิ่มสินค้า</div>
+                </ModalHeader>
+                <div className='h-[450px] p-5'>
+                    <div className='overflow-y-auto h-full'>
+                        {[...Array(100)].map((_, index) => (
+                            <div key={index} className='bg-primary p-3 mb-3'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo, atque quisquam mollitia laborum molestias voluptatum
+                                sint? Accusantium distinctio doloremque earum nisi. Asperiores, officiis quibusdam earum tempore fugiat iusto deleniti
+                                temporibus!{' '}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className='h-[80px] p-5'>
+                    <div className='flex justify-end'>
+                        <Button className='bg-white text-black mr-5' onClick={handleCloseModal}>
+                            ยกเลิก
+                        </Button>
+                        <Button onClick={handleCloseModal}>เพิ่ม</Button>
+                    </div>
                 </div>
             </Modal>
         </div>
